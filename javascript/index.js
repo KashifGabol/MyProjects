7512b515da5a8e24d6b5699ce930222320c934eb
KashifGabol <kashifgabol512@gmail.com>
2024-10-11 16:40:33 +0500

    done

--- javascript/index.js
+++ javascript/index.js
@@ -1,54 +1,5 @@
 
-// nav open and close
-document.querySelector("#open-nav-menu").addEventListener("click", function()
-{
-    document.querySelector("header nav .wrapper").classList.add("nav-open");
-})
-
-document.querySelector("#close-nav-menu").addEventListener("click", function(){
-document.querySelector("header nav .wrapper").classList.remove("nav-open");
-})
-
-// Greeting
-
-// Fucntion for celcus to fohrnhiet
-
-function celcusToFohr (temperatur){
-    let fahr = (temperatur * 9/5)  + 32;
-    return fahr;
-}
-
-const greetingText = "Good Morning";
-const userLocation = "New York";
-const weatherCondition = "sunny";
-let temperatur  = 25;
-let celsiusText = `The weather is ${weatherCondition} in ${userLocation} and it's ${temperatur}°C outside.`;
-let fahrText = `The weather is ${weatherCondition} in ${userLocation} and it's ${celcusToFohr (temperatur).toString()}°F outside.`;
-
-document.querySelector("#weather").innerHTML= greetingText;
-document.querySelector("p#weather").innerHTML = celsiusText;
-
-// toggle button celsius to fahrnhight
-
-document.querySelector(".weather-group").addEventListener("click", function(e)
-{
-    if (e.target.id == "celsius"){
-        document.querySelector("p#weather").innerHTML = celsiusText;           
-    } else if (e.target.id = "fahr"){
-        document.querySelector("p#weather").innerHTML = fahrText;
-    }
-
-});
-
-
-
-setInterval(() => {
-    let localTime = new Date ();
-document.querySelector("span[data-time=hours]").textContent = localTime.getHours().toString().padStart(2, 0);
-document.querySelector("span[data-time=minutes]").textContent = localTime.getMinutes().toString().padStart(2,0);
-document.querySelector("span[data-time=seconds]").textContent = localTime.getSeconds().toString().padStart(2, 0);
-
-}, 1000);
+// gallery const
 
 const imageGallery = [
     {
@@ -65,7 +16,72 @@ const imageGallery = [
     }
 ];
 
-// let mainImage = document.querySelector("#gallery > img");
+// manu function 
+function manueHandler(){
+    document.querySelector("#open-nav-menu").addEventListener("click", function()
+    {
+        document.querySelector("header nav .wrapper").classList.add("nav-open");
+    })
+    
+    document.querySelector("#close-nav-menu").addEventListener("click", function(){
+    document.querySelector("header nav .wrapper").classList.remove("nav-open");
+    })
+}
+// Fucntion for celcus to fohrnhiet
+function celcusToFohr (temperatur){
+    let fahr = (temperatur * 9/5)  + 32;
+    return fahr;
+}
+
+// Greeting Handler
+function greetingHandler(){
+    let currentHours = new Date().getHours();
+    let greetingText ;
+    if(currentHours < 12){
+    greetingText = "Good Morning";
+    } else if(currentHours < 19){
+        greetingText = "Good Afternoon";
+    } else if(currentHours < 24){
+        greetingText = "Good Evening";
+    } else {
+        greetingText = "Welcome";
+    }
+    
+    const userLocation = "New York";
+    const weatherCondition = "sunny";
+    let temperatur  = 25;
+    let celsiusText = `The weather is ${weatherCondition} in ${userLocation} and it's ${temperatur}°C outside.`;
+    let fahrText = `The weather is ${weatherCondition} in ${userLocation} and it's ${celcusToFohr (temperatur).toString()}°F outside.`;
+    
+    // document.querySelector("#weather").innerHTML= greetingText;
+    document.querySelector("p#weather").innerHTML = celsiusText;
+    document.querySelector("#greeting").innerHTML = greetingText;
+    document.querySelector(".weather-group").addEventListener("click", function(e)
+{
+    if (e.target.id == "celsius"){
+        document.querySelector("p#weather").innerHTML = celsiusText;           
+    } else if (e.target.id = "fahr"){
+        document.querySelector("p#weather").innerHTML = fahrText;
+    }
+
+});
+}
+
+// colock handler
+
+function cloclHandler (){
+    setInterval(() => {
+        let localTime = new Date ();
+    document.querySelector("span[data-time=hours]").textContent = localTime.getHours().toString().padStart(2, 0);
+    document.querySelector("span[data-time=minutes]").textContent = localTime.getMinutes().toString().padStart(2,0);
+    document.querySelector("span[data-time=seconds]").textContent = localTime.getSeconds().toString().padStart(2, 0);
+    
+    }, 1000);
+}
+
+// gallery handler
+function galleryHandler(){
+    // let mainImage = document.querySelector("#gallery > img");
 // mainImage.src=imageGallery[0].src;
 // mainImage.alt=imageGallery[0].alt;
 
@@ -99,3 +115,9 @@ imageGallery.forEach(function (image, index){
     // }
 
 });
+}
+
+manueHandler();
+greetingHandler();
+cloclHandler ();
+galleryHandler();
