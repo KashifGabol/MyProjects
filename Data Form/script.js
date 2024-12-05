d20111df667cc7e6e535937659a4b1a124c00230
KashifGabol <kashifgabol512@gmail.com>
2024-12-05 14:41:17 +0500

    done

--- Data Form/script.js
+++
@@ -1,8 +0,0 @@
-    // Preview uploaded profile picture
-    function previewImage(event) {
-        const reader = new FileReader();
-        reader.onload = function() {
-            const imgElement = document.getElementById('profileImage');
-            imgElement.src = reader.result;
-        }
-        reader.readAsDataURL(event.target.files[0]);
