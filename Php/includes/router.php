370b0f9cbcd77ad249d36b2d5698a98063284c7f
KashifGabol <kashifgabol512@gmail.com>
2024-12-06 22:12:37 +0500

    done

--- Php/includes/router.php
+++ Php/includes/router.php
@@ -1,1 +1,13 @@
 <?php
+
+declare(strict_types=1);
+
+function dispach(string $uri, string $method): void
+{
+    // 1) normalize the URI: GET /guestbook -> routes /guestbook_get.php
+    // 2) GET|POST - return 404
+    // 3) file path - php file path
+    // 4) if this file exists, if not 404
+    // 5) handle the routes by including php files
+
+}
